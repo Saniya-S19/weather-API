076e7a8aadc18fffdd7b0cd292bc73e9b577c1d7
Saniya <ssaniya1902@gmail.com>
2025-09-03 23:06:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+const apikey="71c7f75838c0907ce4c48860c6bcc920";
+async function getWeather() 
+{
+    const city = document.getElementById("getdata").value;
+    if(city.trim() === " ") 
+        {
+            alert("Enter city name");
+            return;
+    }
+
+const url=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}&units=metric`;
+
+try {
+    const response = await fetch(url);
+    const data = await response.json();
+
+    if(data.cod === "404") {
+        document.getElementById("WeatherResult").innerHTML = "City not found";
+        return;
+    }
+
+    document.getElementById("WeatherResult").innerHTML =`
+      <h2>${data.name}, ${data.sys.country}</h2>
+      <p>🌡 Temperature: ${data.main.temp}°C</p>
+      <p>☁ Weather: ${data.weather[0].description}</p>
+      <p>💧 Humidity: ${data.main.humidity}%</p>
+      <p>🌬 Wind Speed: ${data.wind.speed} m/s</p>`;
+  } 
+  catch (error) 
+  {
+    document.getElementById("WeatherResult").innerHTML = "Error fetching data!";
+  }
+}
